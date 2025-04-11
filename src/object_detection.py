1f65c535032824c2eb0a02e742aea8d463b4c29b
Aryan Tandon <aryantandon2323@gmail.com>
2025-04-11 20:35:41 +0530

    Update object_detection.py

--- src/object_detection.py
+++ src/object_detection.py
@@ -4,19 +4,17 @@ from PIL import Image
 
 model = YOLO("yolov8s.pt") 
 
-def detect_objects(pil_image):
+def detect_objects(pil_image, conf_threshold=0.6):
     """
     Runs YOLOv8 on a PIL image and returns detected objects.
     Returns: 
     - List of dicts with class name, confidence, and bounding box
     - Annotated image     
     """
-    # Convert PIL to np.array
     img_array = np.array(pil_image.convert("RGB"))
     
-    # Run inference
-    results = model(img_array, verbose=False)
-
+    # inference with confidence threshold
+    results = model(img_array, conf=conf_threshold, verbose=False)
     objects = []
     for result in results:
         boxes = result.boxes
@@ -25,13 +23,11 @@ def detect_objects(pil_image):
             name = result.names[cls_id]
             conf = float(box.conf[0].item())
             x1, y1, x2, y2 = map(int, box.xyxy[0].tolist())
-
             objects.append({
                 "class": name,
                 "confidence": conf,
                 "box": [x1, y1, x2, y2]
             })
-
     annotated_array = results[0].plot()
     annotated_image = Image.fromarray(annotated_array)
-    return objects,annotated_image
+    return objects, annotated_image
